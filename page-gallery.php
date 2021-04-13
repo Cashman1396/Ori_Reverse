e83303894b3bf66b3b9ae63c2552bda0679a2da2
Cashman1396 <Chrisashman96@gmail.com>
2021-04-13 15:51:36 -0400

    learning php

---
+++ page-gallery.php
@@ -0,0 +1,29 @@
+<?php
+/*Template Name: Image gallery
+*/
+
+get_header();
+?>
+
+
+<?php while ( have_posts() ) : the_post(); ?> <!--queries the projects-page.php-->
+	
+
+
+
+     <?php the_content() ?> <!--Grabs any content added to the Editor-->
+    <?php endwhile; // end of the loop. ?>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); 
+?>
