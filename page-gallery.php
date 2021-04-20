32610dc11b8380a264617e420b6583a820fd1822
Cashman1396 <Chrisashman96@gmail.com>
2021-04-20 10:35:49 -0400

    did a few changes

--- page-gallery.php
+++ page-gallery.php
@@ -7,7 +7,7 @@ get_header();
 
 
 <?php while ( have_posts() ) : the_post(); ?> <!--queries the projects-page.php-->
-
+<div class="gallery">
 <?php 
 $images = get_field('gallery');
 if( $images ): ?>
@@ -20,7 +20,7 @@ if( $images ): ?>
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
-
+        </div>
      <?php the_content() ?> <!--Grabs any content added to the Editor-->
     <?php endwhile; // end of the loop. ?>
 
