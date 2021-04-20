e232de1e928debbf4d8bf0babbacfd1a88dcd1b3
Cashman1396 <Chrisashman96@gmail.com>
2021-04-20 12:23:04 -0400

    added divs to files

--- page.php
+++ page.php
@@ -6,8 +6,10 @@ get_header();
 
 <?php while ( have_posts() ) : the_post(); ?> <!--queries the projects-page.php-->
 	
+<div class="excerpt">
      <?php the_content() ?> <!--Grabs any content added to the Editor-->
     <?php endwhile; // end of the loop. ?>
+</div>
 
 
 
