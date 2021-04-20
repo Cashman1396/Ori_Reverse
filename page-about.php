c3461f6d2a302661b435e8592c6c5be673aa254c
Cashman1396 <Chrisashman96@gmail.com>
2021-04-20 09:49:42 -0400

    added about page

--- page-about.php
+++ page-about.php
@@ -1,5 +1,7 @@
 <?php 
 
+/* Template Name: About */
+
 get_header(); 
 ?>
 
--- page-gallery.php
+++ page-gallery.php
@@ -8,6 +8,18 @@ get_header();
 
 <?php while ( have_posts() ) : the_post(); ?> <!--queries the projects-page.php-->
 
+<?php 
+$images = get_field('gallery');
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image_ID ): ?>
+            <li>
+                <?= wp_get_attachment_image($image_ID, "full") ?>
+                
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
      <?php the_content() ?> <!--Grabs any content added to the Editor-->
     <?php endwhile; // end of the loop. ?>
