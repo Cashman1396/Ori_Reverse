22b24b48e0645b60d8dd1a521b3d8716d762c799
Cashman1396 <Chrisashman96@gmail.com>
2021-05-25 17:24:37 -0400

    made more additions

--- page-gameslibrary.php
+++ page-gameslibrary.php
@@ -6,12 +6,14 @@ get_header();
 ?>
 
 
+<?php while ( have_posts() ) : the_post(); ?> <!--queries the projects-page.php-->
+
+<?php the_content() ?> <!--Grabs any content added to the Editor-->
+<?php endwhile; // end of the loop. ?>
 
 
-<p>By Chris Ashman 2021</p>
-
-<?php wp_footer(); ?>
-</body>
 
 
-</html>
+<div class="footer">
+<?php get_footer(); ?>
+</div>
